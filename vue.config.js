f0ca0a234e6a3c130c585f6544bd39cf9c3b617c
vita2333 <lp_vitadolce@163.com>
2020-01-06 18:27:14 +0800

    🌸 拆分多页面

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const titles = require('./title.js')
+const glob = require('glob')
+const pages = {}
+
+glob.sync('./src/pages/**/main.js').forEach(path => {
+	const chunk = path.split('./src/pages/')[1].split('/main.js')[0]
+	pages[chunk] = {
+		entry: path,
+		template: 'public/index.html',
+		title: titles[chunk],
+		chunks: ['chunk-vendors', 'chunk-common', chunk]
+	}
+})
+module.exports = {
+	pages,
+	chainWebpack: config => config.plugins.delete('named-chunks'),
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:8080',
+				changeOrigin: true,
+				pathRewrite: { '^/api': '' }
+			}
+		}
+	}
+}
