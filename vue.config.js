d05395af76e015ae11bdb098e8ad3550d55702d3
vita2333 <lp_vitadolce@163.com>
2020-01-09 19:12:32 +0800

    🌸 引入webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,34 @@
-const titles = require('./title.js')
-const glob = require('glob')
-const pages = {}
+const titles = require("./title.js");
+const glob = require("glob");
+const pages = {};
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const report = process.env.npm_config_report;
 
-glob.sync('./src/pages/**/main.js').forEach(path => {
-	const chunk = path.split('./src/pages/')[1].split('/main.js')[0]
-	pages[chunk] = {
-		entry: path,
-		template: 'public/index.html',
-		title: titles[chunk],
-		chunks: ['chunk-vendors', 'chunk-common', chunk]
-	}
-})
+glob.sync("./src/pages/**/main.js").forEach(path => {
+  const chunk = path.split("./src/pages/")[1].split("/main.js")[0];
+  pages[chunk] = {
+    entry: path,
+    template: "public/index.html",
+    title: titles[chunk],
+    chunks: ["chunk-vendors", "chunk-common", chunk]
+  };
+});
 module.exports = {
-	pages,
-	chainWebpack: config => config.plugins.delete('named-chunks'),
-	devServer: {
-		proxy: {
-			'/api': {
-				target: 'http://127.0.0.1:8080',
-				changeOrigin: true,
-				pathRewrite: { '^/api': '' }
-			}
-		}
-	}
-}
+  pages,
+  chainWebpack: config => config.plugins.delete("named-chunks"),
+  configureWebpack: config => {
+    if (report) {
+      config.plugins.push(new BundleAnalyzerPlugin());
+    }
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" }
+      }
+    }
+  }
+};
